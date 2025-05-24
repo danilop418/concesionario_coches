c12aa604049509c88eeb4bc014bccb849da99d94
Dani361 <l00p3zz9@gmail.com>
2025-05-24 11:47:06 +0200

    #1-Resuelta, he añadido un cliente, un vehículo y una venta nueva.

--- src/coches/Main.java
+++ src/coches/Main.java
@@ -8,7 +8,13 @@ public class Main {
         Vehiculo vehiculo = new Vehiculo(" zxl5678 ", " BMW ", " 350 ", " azul ", " gasolina ", 200);
         Venta venta = new Venta(123, LocalDate.now(), cliente, vehiculo, 54454.23, " tarjeta ");
 
+        Cliente cliente2 = new Cliente(" 808080M ", " Javi ", " Martin ", " Calle inventada 2 ");
+        Vehiculo vehiculo2 = new Vehiculo(" zxl5452 ", " Mercedes ", " 570 ", " rojo ", " diesel ", 200);
+        Venta venta2 = new Venta(123, LocalDate.now().plusDays(1), cliente2, vehiculo2, 54454.23, " tarjeta ");
+
         System.out.println(venta);
+        System.out.println(venta2);
+
     }
 }
 
