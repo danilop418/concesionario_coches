f68a503ec5665dd58f76c41ae9fc955e6decfb34
Dani361 <l00p3zz9@gmail.com>
2025-05-24 11:34:18 +0200

    Version 2: He cambiado la imagen del diagrama a src  y he borrado el directorio diagrama.

--- src/coches/Main.java
+++ src/coches/Main.java
@@ -4,10 +4,11 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente(" 505060M ", " Dani ", " Lopez "," Calle inventada ");
-        Vehiculo vehiculo = new Vehiculo(" zxl5678 ", " BMW ", " 350 ", " azul ", " gasolina ",200);
-        Venta venta = new Venta(123, LocalDate.now(),cliente,vehiculo,54454.23," tarjeta ");
+        Cliente cliente = new Cliente(" 505060M ", " Dani ", " Lopez ", " Calle inventada ");
+        Vehiculo vehiculo = new Vehiculo(" zxl5678 ", " BMW ", " 350 ", " azul ", " gasolina ", 200);
+        Venta venta = new Venta(123, LocalDate.now(), cliente, vehiculo, 54454.23, " tarjeta ");
 
         System.out.println(venta);
     }
 }
+
